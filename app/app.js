45dce1b05100c7027020de01904e7cab5bd87eae
Stephane Bruckert <contact@stephanebruckert.com>
2015-09-17 23:43:53 +0200

    removed view2

--- app/app.js
+++ app/app.js
@@ -4,9 +4,12 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
-  'myApp.view2',
   'myApp.version'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.otherwise({
+      redirectTo: '/view1'
+    });
+  }
+]);
